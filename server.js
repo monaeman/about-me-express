508b9353943fb50be2fc3dec05e1d5dfe802f11f
monaeman <mona.eman2018@gmail.com>
2023-08-23 13:26:11 -0400

    routes

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.get("/", (req, res) => {
       <li><a href="/travel">Countries I Want to Visit</a></li>
       <li><a href="/fear">Things I Have Phobia From</a></li>
       <li><a href="/sports">Sports I Watch</a></li>
-      <li><a href="/sports">Sports I Watch</a></li>
-      <li><a href="/sports">Sports I Watch</a></li>
-      <li><a href="/sports">Sports I Watch</a></li>
+      <li><a href="/education">Degree</a></li>
+      <li><a href="/Car">Favorite cars</a></li>
+      <li><a href="/dessert">Favorite Dessert</a></li>
     </ul>
   `);
 });
@@ -97,6 +97,40 @@ app.get("/sports", (req, res) => {
   res.send(`
     <h1>Sports I watch</h1>
     <p>I like to watch world soccer matches and the swimming Olympic races.</p>
+    <ul>
+      <li><a href="/travel">Countries I Want to Visit</a></li>
+      <li><a href="/fear">Things I Have Phobia From</a></li>
+      <li><a href="/">Home Page Profile</a></li>
+    </ul>
+  `);
+});
+app.get("/dessert", (req, res) => {
+  res.send(`
+      <h1>favorite dessert</h1>
+      <p>I like banana pudding </p>
+      <ul>
+        <li><a href="/travel">Countries I Want to Visit</a></li>
+        <li><a href="/fear">Things I Have Phobia From</a></li>
+        <li><a href="/">Home Page Profile</a></li>
+      </ul>
+    `);
+});
+app.get("/car", (req, res) => {
+  res.send(`
+      <h1>favorite car</h1>
+      <p> My favorite car is a range rover</p>
+      <ul>
+        <li><a href="/travel">Countries I Want to Visit</a></li>
+        <li><a href="/fear">Things I Have Phobia From</a></li>
+        <li><a href="/">Home Page Profile</a></li>
+      </ul>
+    `);
+});
+
+app.get("/education", (req, res) => {
+  res.send(`
+    <h1>Degree I have</h1>
+    <p>I have a Bachelor degree in physics</p>
     <ul>
       <li><a href="/travel">Countries I Want to Visit</a></li>
       <li><a href="/fear">Things I Have Phobia From</a></li>
